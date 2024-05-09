3725a9390d9460a3cbd6a4d9ab5c5da578a2fd26
Indigo Tech <yungboiiindigo@gmail.com>
2024-05-09 18:09:24 -0500

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,14 @@
+Rails.application.routes.draw do
+  devise_for :users
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
+  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  get "up" => "rails/health#show", as: :rails_health_check
+  
+  resources :profile, only: [:show], param: :user_id
+  authenticated :user do
+    root to: "explore#index", as: :authenticated_user_root
+  end
+  # Defines the root path route ("/")
+  root "home#index"
+end
