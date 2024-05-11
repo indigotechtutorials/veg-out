b4fb1a0839b8d69ac251f9c7f6c5368b6a364590
Indigo Tech <yungboiiindigo@gmail.com>
2024-05-11 13:51:12 -0500

    styling imrpovements to chat show yours vs there chat colors

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -1,4 +1,5 @@
 class ChatsController < ApplicationController
+  before_action :set_chat, :authorize_chat_user, only: [:show]
   def create
     @to_user = User.find(params[:to_user_id])
     @chat_request = ChatRequest.create(user: current_user, to_user: @to_user)
@@ -6,6 +7,16 @@ class ChatsController < ApplicationController
   end
 
   def show
+    
+  end
+
+  private
+
+  def set_chat
     @chat = Chat.find(params[:id])
+  end
+
+  def authorize_chat_user
+    redirect_to root_path, alert: "You are not authorized to view this chat sorry :P" unless @chat.chat_users.where(user: current_user).first.present?
   end
 end
--- app/javascript/controllers/chat_messages_controller.js
+++ app/javascript/controllers/chat_messages_controller.js
@@ -2,13 +2,21 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="chat-messages"
 export default class extends Controller {
-  static targets = "message"
+  static targets = ["message"]
+  static values = { currentUserId: String }
   connect() {
     this.scrollToBottom()
   }
 
-  messageTargetConnected() {
+  messageTargetConnected(target) {
     this.scrollToBottom()
+    console.log(target.dataset.userId)
+    if (target.dataset.userId == this.currentUserIdValue) {
+      console.log("Same user")
+      target.classList.add("bg-indigo-500", "text-indigo-100")
+    } else {
+      target.classList.add("bg-gray-100")
+    }
   }
 
   scrollToBottom() {
